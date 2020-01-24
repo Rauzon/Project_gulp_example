2585cef38ab0e3de6bc0638b0c4a80ab4f2bdf99
Crispyss <vladby86@gmail.com>
2020-01-24 22:26:54 +0300

    completed five blocks

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
 
 
 gulp.task('scss', function(){
     return gulp.src('app/scss/**/*.scss')
-        .pipe(sass())
+        .pipe(sass({outputStyle:"compressed"}))
+        .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream:true}))
 });
 
 gulp.task('html', function(){
     return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('script', function(){
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('js', function(){
+    return gulp.src([
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js/'))
         .pipe(browserSync.reload({stream:true}))
 });
 
@@ -26,7 +46,8 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function(){
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
     gulp.watch('app/*.html', gulp.parallel('html'));
+    gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
 
-gulp.task('default', gulp.parallel('browser-sync','watch'));
+gulp.task('default', gulp.parallel('scss','js','browser-sync','watch'));
 
